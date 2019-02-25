f72c6d336bff3e388ff700564c6174a20d9bdfc8
Michael Weber <webermn15@gmail.com>
2019-02-25 17:05:30 -0600

    please im begging you

--- server.js
+++ server.js
@@ -3,11 +3,12 @@ const app = express();
 const path = require('path');
 
 process.env.PWD = process.cwd();
+console.log('PWD process: ', process.env.PWD);
 
 const port = process.env.PORT || 3001;
 
-app.use(express.static(path.join(process.env.PWD+'/dist/build')));
-app.use(express.static(path.join(__dirname+'/dist/build')));
+app.use(express.static(path.join(process.env.PWD, '/dist/build')));
+app.use(express.static(path.join(__dirname, '/dist/build')));
 
 app.get("*", function (req, res) {
   res.sendFile(path.join(__dirname+'/dist/build/', 'index.html'));
